f747939951ad9eba84e115e773dadaa0d3a74848
Aaron <antvaaron@gmail.com>
2025-11-27 10:57:12 +0800

    fix(design): hide btns group in default

--- src/designs/components/BtnsGroup.tsx
+++ src/designs/components/BtnsGroup.tsx
@@ -5,6 +5,12 @@ export interface BtnsGroupProps extends GroupProps {}
 
 export const BtnsGroup = (props: BtnsGroupProps) => {
   return (
-    <Group data-element-type="btns-group" width={0} height={0} {...props} />
+    <Group
+      data-element-type="btns-group"
+      width={0}
+      height={0}
+      {...props}
+      display="none"
+    />
   );
 };
